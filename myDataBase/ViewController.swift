1e7b09f7b14733ff5cf7aea3266cd5bbf311b3a2
Muhammed Najm <vec.xyz.tor@gmail.com>
2020-01-09 12:27:38 +0300

    database setup completed

--- myDataBase/ViewController.swift
+++ myDataBase/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 import Firebase
+
 class ViewController: UIViewController {
     var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ref = Database.database().reference()
-        
        
     }
 
